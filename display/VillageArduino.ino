9b4682ec542cb91823e761ccc00e5ce65f6c5da8
Austin <amreuland@gmail.com>
2014-11-13 14:06:29 -0600

    Arduino Code for the Village

---
+++ display/VillageArduino.ino
@@ -0,0 +1,79 @@
+
+long timer = 0;
+int hallCount = 0;
+float hallAvg = 0;
+int lastReadVal = 0;
+int reedVal = 0;
+
+int i;
+
+int ledPins[8] = {10,11,12,13,A0,A1,A2,A3};
+
+int ledStates[8];
+
+void setup(){
+  pinMode(9, INPUT);
+
+  for(i = 0; i < 8; i++){
+    pinMode(ledPins[i], OUTPUT);
+  }
+
+  // TIMER SETUP- the timer interrupt allows preceise timed measurements of the reed switch
+  //for mor info about configuration of arduino timers see http://arduino.cc/playground/Code/Timer1
+  cli();//stop interrupts
+
+  //set timer1 interrupt at 1kHz
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;
+  // set timer count for 5khz increments
+  OCR1A = 399;// = (1/5000)/((1/(16x10^6))x8) - 1
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS11 bit for 8 prescaler
+  TCCR1B |= (1 << CS11);   
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+
+  sei();//allow interrupts
+  //END TIMER SETUP
+
+  Serial.begin(9600);
+}
+
+
+ISR(TIMER1_COMPA_vect) {
+  reedVal = digitalRead(9);
+  if(reedVal){
+    if(lastReadVal == 0){
+      hallCount += 1;
+    }
+  }else{
+    if(lastReadVal == 1){
+      hallCount += 1;
+    }
+  }
+  lastReadVal = reedVal;
+  timer += 1;
+  if(timer > 1000){
+    timer = 0;
+    hallAvg = hallCount;
+    hallCount = 0;
+  }
+}
+
+void loop(){
+  Serial.print(String(hallAvg) + "\n");
+  ledStates[0] = hallAvg > 25;
+  ledStates[1] = hallAvg > 50;
+  ledStates[2] = hallAvg > 100;
+  ledStates[3] = hallAvg > 135;
+  ledStates[4] = hallAvg > 165;
+  ledStates[5] = hallAvg > 180;
+  ledStates[6] = hallAvg > 200;
+  ledStates[7] = hallAvg > 230;
+
+  for(i = 0; i < 8; i++){
+    digitalWrite(ledPins[i], ledStates[i]);
+  }
+}
